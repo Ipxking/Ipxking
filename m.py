8c393d7be3d39c0140a5359d169d04679dd8359a
Ipxking <mjdegjltele@gmail.com>
2024-05-25 23:18:58 +0530

    Add files via upload

--- m.py
+++ m.py
@@ -235,7 +235,7 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 181:
+            if time > 240:
                 response = "Error: Time interval must be less than 180."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
